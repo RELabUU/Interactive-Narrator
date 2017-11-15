3f0c9fccd8017e6b5b70a982733446cebdf81258
Govert-Jan Slob <govertjan@gmail.com>
2017-11-15 11:33:49 +0100

    added /about page

--- app/app.py
+++ app/app.py
@@ -92,6 +92,10 @@ def demo():
 def homepage():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     form = ContactForm(request.form)
